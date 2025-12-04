e738465d2bb9c04fa52fdd6b43327c5e2622b66a
jasmin.hodzic <jasmin.hodzic@kadaster.nl>
2025-12-04 13:20:34 +0100

    javascript cursus

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Jasmin');
+console.log(23);
+
+let firstName = "Jasmin";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
